803b9da4763e5bc51e325ad13c58474b91b81f73
silopirt <julian.sonderegger2005@gmail.com>
2021-11-17 09:19:07 +0100

    worked on TicTacToe

--- src/at/jul/projects/array/TicTacToe.java
+++ src/at/jul/projects/array/TicTacToe.java
@@ -7,10 +7,18 @@ public class TicTacToe {
     public static void main(String[] args){
         char[][] playgroundTicTacToe= new char[3][3];
         Scanner scanner = new Scanner(System.in);
-        int inputGame = scanner.nextInt();
+        System.out.println("Geben sie entweder 1,2 oder 3 ein");
+        int inputGameOnX = scanner.nextInt();
+        System.out.println("Geben sie entweder 1,2 oder 3 ein");
+        int inputGameOnY = scanner.nextInt();
+
+        buildField(playgroundTicTacToe,inputGameOnX,inputGameOnY);
+    }
+
+    private static void buildField(char[][] playgroundTicTacToe,int inputGameI, int inputGameJ) {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                playgroundTicTacToe[i][j] = playgroundTicTacToe[i][j];
+                playgroundTicTacToe[i][j] = playgroundTicTacToe[inputGameI][inputGameJ];
                 if (i == 0) {
                     if (j == 2) {
                         printLineForPlaygroundBreakLine(playgroundTicTacToe[i][j]);
@@ -28,9 +36,7 @@ public class TicTacToe {
                 }
             }
         }
-        System.out.println(inputGame);
     }
-
     private static void printLineForPlayground(char c) {
         System.out.print("|" + c);
     }
