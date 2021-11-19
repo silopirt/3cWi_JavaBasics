45f962118441cfee27947240c6e092a681e9fcdd
silopirt <julian.sonderegger2005@gmail.com>
2021-11-19 11:50:30 +0100

    input finished

--- src/at/jul/projects/array/TicTacToe.java
+++ src/at/jul/projects/array/TicTacToe.java
@@ -4,43 +4,51 @@ import java.io.InputStream;
 import java.util.Scanner;
 
 public class TicTacToe {
-    public static void main(String[] args){
-        char[][] playgroundTicTacToe= new char[3][3];
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Geben sie entweder 1,2 oder 3 ein");
-        int inputGameOnX = scanner.nextInt();
-        System.out.println("Geben sie entweder 1,2 oder 3 ein");
-        int inputGameOnY = scanner.nextInt();
+    public static void main(String[] args) {
+        int[][] playgroundTicTacToe = new int[3][3];
 
-        buildField(playgroundTicTacToe,inputGameOnX,inputGameOnY);
+        printField(playgroundTicTacToe);
+
     }
 
-    private static void buildField(char[][] playgroundTicTacToe,int inputGameI, int inputGameJ) {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                playgroundTicTacToe[i][j] = playgroundTicTacToe[inputGameI][inputGameJ];
-                if (i == 0) {
-                    if (j == 2) {
-                        printLineForPlaygroundBreakLine(playgroundTicTacToe[i][j]);
+    private static void printField(int[][] field) {
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 9; i++) {
+            System.out.println("Geben sie 1 für Spieler 1 und 2 für Spieler 2 ein");
+            int inputPlayer = scanner.nextInt();
+
+            System.out.println("Geben sie entweder 0,1 oder 2 ein");
+            int inputRow = scanner.nextInt();
+            System.out.println("Geben sie entweder 0,1 oder 2 ein");
+            int inputCol = scanner.nextInt();
+
+            if(inputPlayer == 1){
+                field[inputCol][inputRow] = 1;
+            }else if(inputPlayer == 2){
+                field[inputCol][inputRow] = 2;
+            }
+
+            for (int row = 0; row < 3; row++) {
+                String output = "";
+                for (int col = 0; col < 3; col++) {
+                    if (field[row][col] == 1) {
+                        output += "x";
+                    } else if (field[row][col] == 2) {
+                        output += "o";
                     } else {
-                        printLineForPlayground(playgroundTicTacToe[i][j]);
+                        output += "|  ";
                     }
-                } else if (i == 1) {
-                    if (j == 2) {
-                        printLineForPlaygroundBreakLine(playgroundTicTacToe[i][j]);
-                    } else {
-                        printLineForPlayground(playgroundTicTacToe[i][j]);
-                    }
-                } else {
-                    printLineForPlayground(playgroundTicTacToe[i][j]);
                 }
+                    switch (output) {
+                        case "xxx":
+                            System.out.println("Spieler 1 hat gewonnen");
+                            break;
+                        case "ooo":
+                            System.out.println("Spieler 2 hat gewonnen");
+                            break;
+                    }
+                System.out.println(output);
             }
         }
-    }
-    private static void printLineForPlayground(char c) {
-        System.out.print("|" + c);
-    }
-    private static void printLineForPlaygroundBreakLine(char c) {
-        System.out.println("|" + c);
     }
 }
