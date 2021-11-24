02d9508feb3f0216ee7970d0d6961c197c976053
silopirt <julian.sonderegger2005@gmail.com>
2021-11-24 09:36:48 +0100

    only thing missing is the check if the inputs are valid

--- src/at/jul/projects/array/TicTacToe.java
+++ src/at/jul/projects/array/TicTacToe.java
@@ -1,4 +1,5 @@
 package at.jul.projects.array;
+import javax.swing.*;
 import java.io.InputStream;
 import java.util.Scanner;
 
@@ -13,53 +14,64 @@ public class TicTacToe {
     private static void printField(int[][] field) {
         Scanner scanner = new Scanner(System.in);
         for (int i = 0; i < 9; i++) {
-            boolean gameEnd = true;
-            System.out.println("Geben sie 1 für Spieler 1 und 2 für Spieler 2 ein");
-            int inputPlayer = scanner.nextInt();
+            boolean gameEnd = false;
 
-            System.out.println("Geben sie entweder 0,1 oder 2 ein");
-            int inputRow = scanner.nextInt();
-            System.out.println("Geben sie entweder 0,1 oder 2 ein");
-            int inputCol = scanner.nextInt();
+            int inputPlayer = 0;
+            int inputRow = 0;
+            int inputCol = 0;
+                try{
+                    System.out.println("Geben sie 1 für Spieler 1 und 2 für Spieler 2 ein");
+                    inputPlayer = scanner.nextInt();
 
-            if(inputPlayer == 1){
-                field[inputCol][inputRow] = 1;
-            }else if(inputPlayer == 2){
-                field[inputCol][inputRow] = 2;
+                    System.out.println("Geben sie entweder 0,1 oder 2 ein für die Reihe");
+                    inputRow = scanner.nextInt();
+                    System.out.println("Geben sie entweder 0,1 oder 2 ein für die Spalte");
+                    inputCol = scanner.nextInt();
+                }catch(Exception e){
+                    System.out.println("Geben sie bitte eine Zahl ein");
+                }
+
+            if (inputPlayer == 1) {
+                field[inputRow][inputCol] = 1;
+            } else if (inputPlayer == 2) {
+                field[inputRow][inputCol] = 2;
             }
 
             for (int row = 0; row < 3; row++) {
                 String output = "";
                 for (int col = 0; col < 3; col++) {
                     if (field[row][col] == 1) {
-                        output += "x";
+                        output += "x ";
                     } else if (field[row][col] == 2) {
-                        output += "o";
+                        output += "o ";
                     } else {
-                        output += "|  ";
+                        output += "| ";
                     }
                 }
-
                 System.out.println(output);
             }
-            if(gameEnd == false){
-                System.out.println("Das Spiel ist beendet");
+            gameEnd = checkIfWinner(field, gameEnd);
+            if (gameEnd == true) {
                 break;
             }
         }
     }
-    private static boolean checkIfWinner(int[][] field){
-        for (int row = 0; row < 3; row++) {
-            if(field[row][0] == field[row][1] && field[row][1]==field[row][2]){
 
+    private static boolean checkIfWinner(int[][] field, boolean gameEnd) {
+        for (int row = 0; row < 3; row++) {
+            if ((field[row][0] == field[row][1] && field[row][1] == field[row][2]) && field[row][0] != 0) {
+                gameEnd = true;
             }
             for (int col = 0; col < 3; col++) {
-                if(field[0][col]== field[1][col] && field[1][col]==field[2][col]){
-
+                if ((field[0][col] == field[1][col] && field[1][col] == field[2][col]) && field[0][col] != 0) {
+                    gameEnd = true;
+                } else if ((field[0][0] == field[1][1] && field[1][1] == field[2][2]) && field[0][0] != 0) {
+                    gameEnd = true;
+                } else if ((field[0][2] == field[1][1] && field[1][1] == field[2][0]) && field[0][2] != 0) {
+                    gameEnd = true;
                 }
-
             }
         }
-        return true;
+        return gameEnd;
     }
 }
