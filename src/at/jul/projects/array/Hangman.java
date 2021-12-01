d10e2fed12c3b4652aad9fcf4573b0f82cc8f259
silopirt <julian.sonderegger2005@gmail.com>
2021-12-01 09:33:53 +0100

    Hangman edited

--- src/at/jul/projects/array/Hangman.java
+++ src/at/jul/projects/array/Hangman.java
@@ -1,7 +1,32 @@
 package at.jul.projects.array;
 
+
+import java.util.Random;
+import java.util.Scanner;
+
 public class Hangman {
     public static void main(String[] args) {
+        String[] wordsToPlayWith = {"Julian", "Hilfe", "Manuel","Milian", "Jonathan", "Serkan"};
+        int numberToChooseWord = getRandomNumber(wordsToPlayWith);
+        char guessOfThePlayer = getInputToGuessWord();
+
+
+
+
 
     }
+    private static int getRandomNumber(String[] whichWordToChoose){
+        Random random = new Random();
+        int numberToChooseWord = random.nextInt(whichWordToChoose.length);
+        System.out.println(numberToChooseWord);
+        return numberToChooseWord;
+    }
+    private static char getInputToGuessWord(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Geben sie ihren Buchstaben ein den sie raten möchten.");
+        char inputForGuessingWord = scanner.next().charAt(0);
+        return inputForGuessingWord;
+    }
+
 }
