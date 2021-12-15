4fe54a9ab970454aee4b64c82de62218601d9fe9
silopirt <julian.sonderegger2005@gmail.com>
2021-12-15 12:29:39 +0100

    finished the loading state

--- src/at/jul/projects/oop/remote/Remote.java
+++ src/at/jul/projects/oop/remote/Remote.java
@@ -1,10 +1,23 @@
 package at.jul.projects.oop.remote;
 import at.jul.projects.oop.remote.objects.*;
 
+import java.util.Scanner;
+
 
 public class Remote {
     public static void main(String[] args) {
+        RemoteObject remoteObject = new RemoteObject();
+        for (int i = 0; i < 2; i++) {
+            Scanner scanner = new Scanner(System.in);
+            int input = scanner.nextInt();
+            if (input >=0 && input<=100) {
+                RemoteBattery battery = new RemoteBattery(input);
+                remoteObject.addRemoteBattery(battery);
+            }
+        }
+        int loadingState = 0;
 
-
+        loadingState = remoteObject.getLoadingState();
+        System.out.println(loadingState);
     }
 }
