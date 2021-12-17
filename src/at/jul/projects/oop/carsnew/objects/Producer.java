6c6bfd6ff56f57ec0f65e2b3633e37a14fdfa009
silopirt <julian.sonderegger2005@gmail.com>
2021-12-17 12:22:19 +0100

    started Car excersise

---
+++ src/at/jul/projects/oop/carsnew/objects/Producer.java
@@ -0,0 +1,15 @@
+package at.jul.projects.oop.carsnew.objects;
+
+public class Producer {
+
+    private String country;
+    private String name;
+
+    private double rabatt;
+
+    public Producer(String country, String name, double rabatt) {
+        this.country = country;
+        this.name = name;
+        this.rabatt = rabatt;
+    }
+}
