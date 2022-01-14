a2eab84a905298efe6d95f6fa58b9ff073dda780
silopirt <julian.sonderegger2005@gmail.com>
2022-01-14 12:20:30 +0100

    Caclulator finished

--- src/at/jul/projects/oop/calculator/Calc.java
+++ src/at/jul/projects/oop/calculator/Calc.java
@@ -6,59 +6,80 @@ import java.util.Scanner;
 public class Calc {
 
     private String nameOfCalc;
-    private int numberOne;
-    private int numberTwo;
+    private double numberOne;
+    private double numberTwo;
 
-    public Calc(String nameOfCalc, int numberOne, int numberTwo) {
+    public Calc(String nameOfCalc, double numberOne, double  numberTwo) {
         this.nameOfCalc = nameOfCalc;
         this.numberOne = numberOne;
         this.numberTwo = numberTwo;
     }
 
-    public int calcAddition(){
-        int addition = numberOne + numberTwo;
+    public double calcAddition(){
+        int addition = (int)numberOne + (int)numberTwo;
         return addition;
     }
 
-    public int calcSubtract(){
-        int subtract = 0;
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Input 1 if you want numberOne - numberTwo or input 2 if you want NumberTwo-NumberOne");
-        int whichNumberGetsSubtracted = scanner.nextInt();
+    public double calcSubtract(){
+        double subtract = 0;
+        int whichNumberGetsSubtracted = getWhichNumberGetsSubtDiv("Input 1 if you want numberOne - numberTwo or input 2 if you want NumberTwo-NumberOne");
 
-        if(whichNumberGetsSubtracted == 1){
-            subtract = numberOne-numberTwo;
-        }else if(whichNumberGetsSubtracted == 2){
-            subtract = numberTwo-numberOne;
-        }else{
-            System.out.println("Input a valid number");
-            calcSubtract();
-        }
+        subtract = getSubtract(subtract, whichNumberGetsSubtracted);
 
         return subtract;
     }
 
-    public int calcMultiply(){
-        int multiplikation = numberOne* numberTwo;
-        return multiplikation;
+
+    public double calcMultiply(){
+        double multiplication = numberOne* numberTwo;
+        return multiplication;
     }
 
-    public int calcDivide(){
-        int divide = 0;
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Input 1 if you want numberOne / numberTwo or input 2 if you want NumberTwo/NumberOne");
-        int whichNumberGetsSubtracted = scanner.nextInt();
-
-        if(whichNumberGetsSubtracted == 1){
-            divide = numberOne/numberTwo;
-        }else if(whichNumberGetsSubtracted == 2){
-            divide = numberTwo/numberOne;
-        }else{
-            System.out.println("Input a valid number");
-            calcDivide();
-        }
+    public double calcDivide(){
+        double divide = 0;
+        int whichNumberGetsSubtracted = getWhichNumberGetsSubtDiv("Input 1 if you want numberOne / numberTwo or input 2 if you want NumberTwo/NumberOne");
+        divide = getDivide(divide, whichNumberGetsSubtracted);
 
         return divide;
     }
 
+
+    private double getSubtract(double subtract, int whichNumberGetsSubtracted) {
+        if(whichNumberGetsSubtracted == 1){
+            subtract = (int)numberOne-(int)numberTwo;
+        }else if(whichNumberGetsSubtracted == 2){
+            subtract = (int)numberTwo-(int)numberOne;
+        }else{
+            System.out.println("Input a valid number");
+            calcSubtract();
+        }
+        return subtract;
+    }
+
+    private int getWhichNumberGetsSubtDiv(String s) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(s);
+        return scanner.nextInt();
+    }
+
+    private double getDivide(double divide, int whichNumberGetsSubtracted) {
+        if(whichNumberGetsSubtracted == 1){
+            divide = (double)numberOne/(double)numberTwo;
+        }else if(whichNumberGetsSubtracted == 2){
+            divide = (double)numberTwo/(double)numberOne;
+        }else{
+            System.out.println("Input a valid number");
+            calcDivide();
+        }
+        return divide;
+    }
+
+
+    public double getNumberOne() {
+        return numberOne;
+    }
+
+    public double getNumberTwo() {
+        return numberTwo;
+    }
 }
--- src/at/jul/projects/oop/calculator/Main.java
+++ src/at/jul/projects/oop/calculator/Main.java
@@ -1,12 +1,31 @@
 package at.jul.projects.oop.calculator;
 
+import at.jul.projects.oop.calculator.objects.CurrencyCalc;
+import at.jul.projects.oop.calculator.objects.DistanceOfCarCalc;
+import at.jul.projects.oop.calculator.objects.GeoCalc;
+import at.jul.projects.oop.calculator.objects.TaxCalc;
+
 public class Main {
     public static void main(String[] args) {
         Calc calc = new Calc("Calculator",5,3);
+        DistanceOfCarCalc distanceCalc = new DistanceOfCarCalc("Distance calc",70,5);
+        CurrencyCalc currencyCalc = new CurrencyCalc("CurrencyCalc",70,0.5);
+        GeoCalc geoCalc = new GeoCalc("GeoCalc",4,5,1);
+        TaxCalc taxCalc = new TaxCalc("TaxCalc",10,1.1);
 
-        System.out.println(calc.calcAddition());
-        System.out.println(calc.calcSubtract());
-        System.out.println(calc.calcMultiply());
-        System.out.println(calc.calcDivide());
+        /**
+         * System.out.println(calc.calcAddition());
+         * System.out.println(calc.calcSubtract());
+         * System.out.println(calc.calcMultiply());
+         * System.out.println(calc.calcDivide());
+         *
+         * System.out.println("Man kann noch " +distanceCalc.calcDivide()+ " km fahren.");
+         *
+         * System.out.println(currencyCalc.calcMultiply());
+         *
+         * System.out.println(geoCalc.checkIfRectangleOrCircleArea("1 für Rechteck 2 für Kreis"));
+         **/
+        System.out.println(taxCalc.taxCalc());
+
     }
 }
