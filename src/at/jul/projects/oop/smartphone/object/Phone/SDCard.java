4f26be2be00aa5b454cf84275260457d88b042ac
silopirt <julian.sonderegger2005@gmail.com>
2022-01-21 12:20:31 +0100

    worked smart

--- src/at/jul/projects/oop/smartphone/object/Phone/SDCard.java
+++ src/at/jul/projects/oop/smartphone/object/Phone/SDCard.java
@@ -12,4 +12,19 @@ public class SDCard {
         this.capacity = capacity;
         this.phoneFiles = new ArrayList<>();
     }
+
+    public void saveFile(PhoneFiles phoneFiles){
+        this.phoneFiles.add(phoneFiles);
+    }
+    public int getCapacity(){
+        int freeSpace = this.capacity;
+        for (int i = 0; i < phoneFiles.size(); i++) {
+            freeSpace =freeSpace-phoneFiles.get(i).getSize();
+        }
+        return freeSpace;
+    }
+
+    public List<PhoneFiles> getPhoneFiles() {
+        return phoneFiles;
+    }
 }
--- src/at/jul/projects/oop/smartphone/object/PhoneClass.java
+++ src/at/jul/projects/oop/smartphone/object/PhoneClass.java
@@ -1,4 +1,26 @@
 package at.jul.projects.oop.smartphone.object;
 
+import at.jul.projects.oop.smartphone.object.Phone.SDCard;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PhoneClass {
+private String color;
+private SDCard sdCard;
+
+    public PhoneClass(String color, SDCard sdCard) {
+        this.color = color;
+        this.sdCard = sdCard;
+    }
+    public List getAllFilesInformation(){
+
+        List informationFiles = sdCard.getPhoneFiles();
+        List newListFiles = new ArrayList<>();
+        for (int i = 0; i < informationFiles.size(); i++) {
+
+        }
+        return informationFiles;
+
+    }
 }
