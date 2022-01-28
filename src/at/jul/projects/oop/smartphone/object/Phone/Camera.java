b560d09a9b0c985c580fee08897da8e97186dd5e
silopirt <julian.sonderegger2005@gmail.com>
2022-01-28 12:16:51 +0100

    finished Handy

--- src/at/jul/projects/oop/smartphone/object/Phone/Camera.java
+++ src/at/jul/projects/oop/smartphone/object/Phone/Camera.java
@@ -1,4 +1,23 @@
 package at.jul.projects.oop.smartphone.object.Phone;
 
+import java.util.Scanner;
+
 public class Camera {
+    private String resolution;
+    private PhoneFile phoneFile;
+
+
+    public Camera(String resolution) {
+        this.resolution = resolution;
+    }
+    public PhoneFile makePicture(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("geben sie den filenamen ein.");
+        String getNewFileName = scanner.next();
+        System.out.println("geben sie die Größe ihrer Datei ein.");
+        int getFileSize = scanner.nextInt();
+        PhoneFile phoneFile = new PhoneFile(getNewFileName, ".jpg", getFileSize);
+
+        return phoneFile;
+    }
 }
--- src/at/jul/projects/oop/smartphone/object/Phone/SIM.java
+++ src/at/jul/projects/oop/smartphone/object/Phone/SIM.java
@@ -1,4 +1,19 @@
 package at.jul.projects.oop.smartphone.object.Phone;
 
 public class SIM {
+    private int id;
+    private String number;
+
+    public SIM(int id, String number) {
+        this.id = id;
+        this.number = number;
+    }
+
+    public void makeCall(String number){
+        System.out.println("Rufe Nummer " + number + " an!");
+    }
+
+    public String getNumber() {
+        return number;
+    }
 }
