110c44cebd5e80ba6b6440e167972c58424c7c32
silopirt <julian.sonderegger2005@gmail.com>
2022-01-28 12:16:51 +0100

    finished Handy

--- src/at/jul/projects/oop/smartphone/MainPhone.java
+++ src/at/jul/projects/oop/smartphone/MainPhone.java
@@ -8,16 +8,23 @@ public class MainPhone {
 
         SDCard sdCard = new SDCard(100);
 
+        SIM sim = new SIM(1,"06501231234");
+
+        Camera camera = new Camera("1920x1080");
+
 
         PhoneFile phoneFiles = new PhoneFile("Image",".jpg",3);
         PhoneFile phoneFiles2 = new PhoneFile("Image2",".jpg",3);
 
-        PhoneClass phoneClass = new PhoneClass("rot",sdCard);
+        PhoneClass phoneClass = new PhoneClass("rot",sdCard, sim,camera);
 
         phoneClass.saveFile(phoneFiles);
         phoneClass.saveFile(phoneFiles2);
 
-
+        System.out.println(phoneClass.freeSpace());
+        System.out.println(phoneClass.getAllFilesInformation());
+        phoneClass.makeCall(sim.getNumber());
+        phoneClass.takePicture();
         System.out.println(phoneClass.getAllFilesInformation());
 
     }
--- src/at/jul/projects/oop/smartphone/object/PhoneClass.java
+++ src/at/jul/projects/oop/smartphone/object/PhoneClass.java
@@ -8,10 +8,14 @@ import java.util.List;
 public class PhoneClass {
 private String color;
 private SDCard sdCard;
+private SIM sim;
+private Camera camera;
 
-    public PhoneClass(String color, SDCard sdCard) {
+    public PhoneClass(String color, SDCard sdCard, SIM sim, Camera camera) {
         this.color = color;
         this.sdCard = sdCard;
+        this.sim = sim;
+        this.camera = camera;
     }
 
 
@@ -20,7 +24,7 @@ private SDCard sdCard;
 
         List newListFiles = new ArrayList<String>();
         for (int i = 0; i < informationFiles.size(); i++) {
-            newListFiles.add(informationFiles.get(0).getInfo());
+            newListFiles.add(informationFiles.get(i).getInfo());
         }
         return newListFiles;
     }
@@ -31,6 +35,14 @@ private SDCard sdCard;
 
     public void saveFile(PhoneFile phoneFile){
         sdCard.saveFile(phoneFile);
+    }
+
+    public void makeCall(String numberSim){
+        sim.makeCall(numberSim);
+    }
+
+    public void takePicture(){
+        sdCard.saveFile(camera.makePicture());
     }
 
 }
