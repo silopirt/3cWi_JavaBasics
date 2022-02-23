ea16ecbf42fd89645aff3120c72dfd05d291effb
silopirt <julian.sonderegger2005@gmail.com>
2022-02-23 09:30:36 +0100

    worked on musicbox

--- src/at/jul/projects/oop/musicbox/MusicboxMain.java
+++ src/at/jul/projects/oop/musicbox/MusicboxMain.java
@@ -11,11 +11,40 @@ public class MusicboxMain {
 
         Plate plateOne = new Plate("Music to be murdered by", "E5");
         Plate plateTwo = new Plate("jkjkj", "r2");
-
+        plateOne.addTitle(titleOne);
+        plateOne.addTitle(titleTwo);
         Magazin magazin = new Magazin("Magazin_One");
 
-        magazin.addRecord(plateOne);
+        boolean application = true;
+        while (application) {
+            System.out.println("-- 1 für Platte hinzufügen \n" + "-- 2 für Platte löschen \n" + "-- 3 für Platte suchen \n" + "-- 4 für Summe der Musik \n"+"-- 5 für Task beenden");
+            Scanner scanner = new Scanner(System.in);
+            int selection = scanner.nextInt();
+            if (selection == 1) {
+                magazin.addRecord(plateOne);
+            } else if (selection == 2) {
+                Plate removePlate = plateCreater(scanner);
+                magazin.removePlate(removePlate);
+            } else if (selection == 3) {
+                Plate searchPlate = plateCreater(scanner);
+                magazin.searchPlate(searchPlate);
+            }else if(selection == 4){
+                System.out.println(magazin.getSumOfMusic());
+            }else {
+                System.out.println("bye");
+                application = false;
+            }
+        }
+    }
 
+    private static Plate plateCreater(Scanner scanner) {
+        System.out.println("Name");
+        String name = scanner.next();
+        System.out.println("Id");
+        String id = scanner.next();
 
+        Plate newPlate = new Plate(name,id);
+
+        return newPlate;
     }
 }
--- src/at/jul/projects/oop/musicbox/objects/Magazin.java
+++ src/at/jul/projects/oop/musicbox/objects/Magazin.java
@@ -14,35 +14,75 @@ public class Magazin {
         this.plates = new ArrayList<>();
     }
 
-    public void addRecord(Plate plate){
-
+    public void addRecord(Plate plate) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Neue Platte designen = 1; Vorhandene einsetzten = 2");
         int desideWhichWay = scanner.nextInt();
 
-        if(desideWhichWay == 1){
-            System.out.println("Wie viele Titel hat die Platte?");
-            int amountOfTitles = scanner.nextInt();
+        if (desideWhichWay == 1) {
+            Plate selfDesign = designNewPlate(scanner);
+            plates.add(selfDesign);
+            getPlateInfo(selfDesign);
 
-            for (int i = 0; i < amountOfTitles; i++) {
-                System.out.println("Name");
-                String nameOfTitle = scanner.next();
-                System.out.println("Länge");
-                double lenghtOfTitle = scanner.nextInt();
-                Title title = new Title(nameOfTitle,lenghtOfTitle);
-                plate.addTitle(title);
-            }
+        } else if (desideWhichWay == 2) {
             plates.add(plate);
-        }else if (desideWhichWay == 2){
-            plates.add(plate);
-        }else{
+            getPlateInfo(plate);
+        } else {
             System.out.println("Fehler");
             return;
         }
 
     }
 
+    public void removePlate(Plate plate) {
+        plates.remove(plate);
+        System.out.println("Platte wurde gelöscht oder nicht gefunden.");
+    }
+
+    public void searchPlate(Plate plate) {
+        for (int i = 0; i < plates.size(); i++) {
+            if (plate.getTitle() == plates.get(i).getTitle() || plate.getId() == plates.get(i).getId()) {
+                System.out.println("Platte wurde gefunden.");
+                plate.getPlateInfo();
+            } else {
+                System.out.println("Platte nicht vorhanden");
+            }
+        }
+    }
+
+    public double getSumOfMusic() {
+        double lenghtOfMagazin = 0;
+        for (int i = 0; i < plates.size(); i++) {
+            lenghtOfMagazin += plates.get(i).getSumOfMusic();
+        }
+        return lenghtOfMagazin;
+    }
+
+    private void getPlateInfo(Plate plate) {
+        plate.getPlateInfo();
+    }
 
 
+    private Plate designNewPlate(Scanner scanner) {
+        System.out.println("name");
+        String plateName = scanner.next();
+        System.out.println("id");
+        String plateId = scanner.next();
+
+        Plate selfDesign = new Plate(plateName, plateId);
+
+        System.out.println("Wie viele Titel hat die Platte?");
+        int amountOfTitles = scanner.nextInt();
+        for (int i = 0; i < amountOfTitles; i++) {
+            System.out.println("Name");
+            String nameOfTitle = scanner.next();
+            System.out.println("Länge");
+            double lenghtOfTitle = scanner.nextDouble();
+
+            Title title = new Title(nameOfTitle, lenghtOfTitle);
+            selfDesign.addTitle(title);
+        }
+        return selfDesign;
+    }
 }
